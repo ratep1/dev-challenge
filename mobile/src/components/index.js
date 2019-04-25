ba8999cd5945ca6aa178401b56dd22ae5c11a75f
pmaric <petar.maric18@gmail.com>
2019-04-25 15:29:58 +0200

    added user and company componentes

--- mobile/src/components/index.js
+++ mobile/src/components/index.js
@@ -1,2 +1,6 @@
 export ErrorScene from './ErrorScene';
 export UserList from './UserList';
+export UserProfile from './UserProfile';
+
+export CompanyList from './CompanyList';
+export CompanyProfile from './CompanyProfile';
