d51c5d6ed0421074a0aa9d64415b7d53707888a8
pmaric <petar.maric18@gmail.com>
2019-04-25 15:27:14 +0200

    apollo setup ngrok

--- mobile/src/apollo.js
+++ mobile/src/apollo.js
@@ -3,7 +3,7 @@ import { AsyncStorage } from "react-native";
 import { persistCache } from "apollo-cache-persist";
 
 // note: this may not work on android, you will probably have to put your IP address here
-const address = "http://localhost:5000/graphql";
+const address = "https://a8108c4b.ngrok.io/graphql";
 
 const cache = new InMemoryCache();
 
